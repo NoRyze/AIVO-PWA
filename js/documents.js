16aeb3cf7de16c4365dad1cb3c31c8954ef0cf39
NoRyze <arnaud.ardouin@hotmail.com>
2026-03-06 16:39:57 +0100

    Fix Error DevTool

--- js/documents.js
+++ js/documents.js
@@ -4,16 +4,19 @@ import { getRole } from "./auth.js";
 export async function loadDocumentsPage(page) {
     const role = getRole();
 
-    const addBtn = page.el.querySelector("btn-upload");
-    const list = page.el.querySelector("#documents-list");
-    
-    // 1. Masquer le bouton d'ajout si user
+    // Framework7 fournit la page dans page.detail.el
+    const pageEl = page.detail.el;
+
+    const addBtn = pageEl.querySelector("#btn-upload");
+    const list = pageEl.querySelector("#documents-list");
+
+    // Masquer upload si user
     if (role !== "admin" && addBtn) {
         addBtn.style.display = "none";
     }
-    
-    // 2. Chargement des documents
-    const docs = await apiFetch("/documents", { method: "GET"});
+
+    // Charger documents
+    const docs = await apiFetch("/documents", { method: "GET" });
 
     list.innerHTML = "";
 
@@ -25,7 +28,7 @@ export async function loadDocumentsPage(page) {
                         <div class="item-title">${doc.fileName}</div>
                         <div class="item-after">
                             <a href="${doc.owner}" target="_blank">Télécharger</a>
-                            ${role === "admin" ? `<button class="delete-doc" data-id"${doc.id}">Suprimer</button>` : ""}
+                            ${role === "admin" ? `<button class="delete-doc" data-id="${doc.id}">Supprimer</button>` : ""}
                         </div>
                     </div>
                 </div>
@@ -33,15 +36,14 @@ export async function loadDocumentsPage(page) {
         `;
     });
 
-    // 3. Gestion supression (admin uniquement)
+    // Suppression admin
     if (role === "admin") {
-       page.el.querySelectorAll(".delete-doc").forEach(btn => {
+        pageEl.querySelectorAll(".delete-doc").forEach(btn => {
             btn.addEventListener("click", async () => {
                 const id = btn.dataset.id;
                 await apiFetch(`/documents/${id}`, { method: "DELETE" });
                 loadDocumentsPage(page); // refresh
             });
-       });
-            
+        });
     }
 }
